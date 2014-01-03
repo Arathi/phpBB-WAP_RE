56001cf088c0cc392f2f98d27c593d13682cdb89
Arathi <ssdfa7@gmail.com>
2014-01-03 19:38:04 +0800

    提交改良打包脚本
    Signed-off-by: Arathi <ssdfa7@gmail.com>

--- package.bat
+++ package.bat
@@ -1,3 +1,3 @@
 del phpBBwap.zip
 7z a -tzip phpBBwap.zip *
-7z d -tzip phpBBwap.zip .git .settings .buildpath .gitignore .project package.bat package.sh README.txt
+7z d -tzip phpBBwap.zip .git .settings .buildpath .gitignore .project package.bat package.sh README.txt phpBBwap.tar.gz
--- package.sh
+++ package.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-rm -f phpBBwap.tar.gz
-tar czvf phpBBwap.tar.gz *
 
+VERSION='REU' #版本标识
+SRC=`pwd` #获取当前目录
+cd ..
+PDATE=`date +%y%m%d%H` #获取打包时间
+TARGET='phpBBwap.'$VERSION'.'$PDATE
+DEST=`pwd`'/'$TARGET
+
+#准备待打包文件
+rm -rf $DEST
+cp -r $SRC $DEST
+cd $DEST
+rm -rf phpBBwap.zip .git .settings .buildpath .gitignore .project package.bat package.sh README.txt phpBBwap.tar.gz
+
+cd ..
+tar czvf $TARGET'.tar.gz' $TARGET
